7cab14b93e73b441ffbed3c7a3e20486c31ad88f
CarlosMascheroni <carlos.mascheroni@gmail.com.com>
2022-08-16 19:42:23 -0300

    Grid

--- WebApplication/ReactApp/src/pages/Blank.js
+++ WebApplication/ReactApp/src/pages/Blank.js
@@ -5,6 +5,10 @@ import {userUrl} from '../utils/http/endpoints'
 import {userUrlTotalRecords} from '../utils/http/endpoints'
 import {urerUrlEdit} from '../utils/http/endpoints'
 import response from '../utils/demo/tableData'
+import { EditIcon, TrashIcon } from '../icons'
+import {
+  Button  
+} from '@windmill/react-ui'
 // import axios, {axiosResponse} from 'axios';
 // import { testUrl } from '../utils/http/endpoints';
 // import AutForm from '../components/form/Models/AutForm';
@@ -26,14 +30,24 @@ function Blank() {
 
     const labels =  ["id","Nombre de Usuario", "Contraseña", "email", "Dirección", "Telefono", "RolId","Fecha Agregado","Fecha Actualizado"]
     const columns = ["id","userName", "password", "email", "address", "phone", "idRole","addRow","updRow"]
+    
  return(
 
     
     <Grid totalRecords = {userUrlTotalRecords}
-          url = {userUrl}
-          urlEdit = {urerUrlEdit}
-          columns = {columns} 
-          labels = {labels}/>
+        url = {userUrl}
+        urlEdit = {urerUrlEdit}
+        columns = {columns} 
+        labels = {labels}>
+          <div className="flex items-center space-x-4">
+            <Button layout="link" size="icon" aria-label="Edit">
+              <EditIcon className="w-5 h-5" aria-hidden="true" />
+            </Button>
+            <Button layout="link" size="icon" aria-label="Delete">
+              <TrashIcon className="w-5 h-5" aria-hidden="true" />
+            </Button>
+          </div>
+    </Grid>
 
 
  )
